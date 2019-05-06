3642c9046bacb6c5c7684243f999904902088620
Jose <cuevacelis@SQLSERVER>
2019-05-05 21:22:38 -0500

    .

---
+++ CapaEntidad/entCiudad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entCiudad
+    {
+        public int idCiudad { get; set; }
+        public String desCiudad { get; set; }
+        public Boolean estPersona { get; set; } 
+    }
+}
---
+++ CapaEntidad/entTipoProducto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entTipoProducto
+    {
+        public int idTipoProducto {get; set;}
+        public String desTipoProducto {get; set;}
+        public Boolean esTipoProducto {get; set;}
+
+    }
+}
