c21777115255e16170008ff45dbd0323937c6ab7
Jose <cuevacelis@SQLSERVER>
2019-05-07 00:06:25 -0500

    Nuevos Cambios Dat

---
+++ CapaAccesoDatos/datCliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    public class datCliente
+    {
+        
+        #region singleton
+        private static readonly datCliente UnicaInstancia = new datCliente();
+        public static datCliente Instancia
+        {
+            get
+            {
+                return datCliente.UnicaInstancia;
+            }
+
+        }
+        #endregion singleton
+
+
+        #region metodos
+        public List<entCliente> ListarCliente()
+        {
+            SqlCommand cmd = null;
+            List<entCliente> lista = new List<entCliente>();
+
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spListaCliente", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    entCliente tipo = new entCliente();
+                    tipo.idCliente = Convert.ToInt16(dr["idCliente"]);
+                    lista.Add(tipo);
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return lista;
+        }
+
+
+
+        #endregion metodos
+    }
+
+}
+
