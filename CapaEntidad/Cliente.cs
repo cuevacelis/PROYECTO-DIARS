08676350c9a03b312a6791d1a910d60c584d89ac
Adrian Alva <antrip_147@live.com>
2019-05-06 22:43:57 -0500

    Agregé la clase Cliente UwU

---
+++ CapaEntidad/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public String nombreCliente { get; set; }
+        public String apellidoCliente { get; set; }
+        public int DNI { get; set; }
+        public Boolean estCliente { get; set; }
+    }
+}
