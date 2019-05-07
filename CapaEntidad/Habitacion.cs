5195c5457494df2f650ae3f0edfc0e72e53d2f99
Adrian Alva <antrip_147@live.com>
2019-05-06 23:28:02 -0500

    Agregué la entidad Habitacion.

---
+++ CapaEntidad/Habitacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Habitacion
+    {
+        public int numeroHabitacion { get; set; }
+        public String descHabitacion { get; set; }
+        public Boolean estHabitacions{ get; set; }
+    }
+}
