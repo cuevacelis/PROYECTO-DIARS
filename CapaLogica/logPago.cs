da981fa07a2fa2ca53f67edea9053e03ea17b7f0
Adrian Alva <antrip_147@live.com>
2019-07-03 04:34:35 -0500

    Pagos hechos

---
+++ CapaLogica/logPago.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaAccesoDatos;
+using CapaEntidad;
+
+namespace CapaLogica
+{
+    public class logPago
+    {
+        #region singleton
+        private static readonly logPago UnicaInstancia = new logPago();
+        public static logPago Instancia
+        {
+            get
+            {
+                return logPago.UnicaInstancia;
+            }
+
+        }
+        #endregion singleton
+
+        #region metodos
+        public List<entPago> ListarPago()
+        {
+            try
+            {
+                List<entPago> lista = datPago.Instancia.ListarPago();
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Boolean InsertarPago(entPago P)
+        {
+            //try
+            //{
+            return datPago.Instancia.InsertarPago(P);
+            //}
+            //catch (Exception e) { throw e; }
+        }
+        #endregion
+    }
+}
---
+++ Maldonado/Controllers/MantenedorPagoController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaEntidad;
+using CapaLogica;
+
+namespace Maldonado.Controllers
+{
+    public class MantenedorPagoController : Controller
+    {
+        // GET: MantenedorPago
+        public ActionResult Index()
+        {
+            return RedirectToAction("ListarPago");
+        }
+
+        [HttpGet]
+        public ActionResult ListarPago()
+        {
+            try
+            {
+                //entUsuario u = (entUsuario)Session["usuario"];
+                ////ViewBag.usuario = u.idCliente.nombreCliente + " " + u.nomUsuario;
+                //if (u.tipo == true)
+                //{
+                List<entPago> lista = logPago.Instancia.ListarPago();
+                ViewBag.lista = lista;
+                return View(lista);
+                //}
+                //else
+                //{
+                //    return RedirectToAction("Index", "Login");
+                //}
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult InsertarPago()
+        {
+            try
+            {
+                //entUsuario u = (entUsuario)Session["usuario"];
+                ////ViewBag.usuario = u.idCliente.nombreCliente + " " + u.nomUsuario;
+                //if (u.tipo == true)
+                //{
+                return View();
+                //}
+                //else
+                //{
+                //    return RedirectToAction("Index", "Login");
+                //}
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult InsertarPago(entPago P, FormCollection frm)
+        {
+            try
+            {
+                //entUsuario u = (entUsuario)Session["usuario"];
+                ////ViewBag.usuario = u.idCliente.nombreCliente + " " + u.nomUsuario;
+                //if (u.tipo == true)
+                //{                
+                Boolean inserta = logPago.Instancia.InsertarPago(P);
+
+                if (inserta)
+                {
+                    return RedirectToAction("ListarPago");
+                }
+                else
+                {
+                    return View(P);
+                }
+                //}
+                //    else
+                //    {
+                //    return RedirectToAction("Index", "Login");
+                //}
+
+            }
+            catch (ApplicationException ex)
+            {
+                return RedirectToAction("InsertarPago", new { mesjExceptio = ex.Message });
+            }
+        }
+    }
+}
